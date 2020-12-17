36bae4873aa6fc9b956e0b10b73ffeae06961d30
Bernardo Garza <bernardo.g.landa@gmail.com>
2020-12-17 17:03:55 -0600

    Created data file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { useState } from 'react';
 import Player from './components/Player';
 import Song from './components/Song';
 import './styles/App.scss';
+import { data } from './util';
 
 function App() {
+  const [songs, setSongs] = useState(data());
   return (
     <div className="App">
-      <h1>Music App</h1>
       <Song />
       <Player />
     </div>
---
+++ src/util.js
@@ -0,0 +1,231 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const data = () => {
+  return [
+    {
+      name: 'Arp 220',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2FGargoyles_Numb_End_-_Arp_220__Arp_299_-_20190527133042437.jpg&width=290&height=290&type=image',
+      artist: "Gargoyle's Numb End",
+      audio: 'https://freemusicarchive.org/track/Arp_220/download',
+      genre: 'Jungle',
+      color: ['#FFC12E', '#D78F1A'],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: 'Abstract Happiness',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2FEgg_Nebula_-_Skyward_EP_-_20140115184318481.jpg&width=290&height=290&type=image',
+      artist: 'Egg Nebula',
+      audio: 'https://freemusicarchive.org/track/05_-_Egg_Nebula_-_Abstract_Happiness/download',
+      genre: 'Jungle',
+      color: ['#34435A', '#C5C2B5'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Vuvle',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2FEgg_Nebula_-_We_Are_Humanity_And_We_Are_Lost_In_The_Cosmic_Infinity_-_20140518210441755.jpg&width=290&height=290&type=image',
+      artist: 'Egg Nebula',
+      audio: 'https://freemusicarchive.org/track/15_Egg_Nebula_-_Vulve/download',
+      genre: 'Jungle',
+      color: ['#05D36B', '#00B0FD'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Magical Forest',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2FEgg_Nebula_-_We_Are_Humanity_And_We_Are_Lost_In_The_Cosmic_Infinity_-_20140518210441755.jpg&width=290&height=290&type=image',
+      artist: 'Egg Nebula',
+      audio: 'https://freemusicarchive.org/track/09_Egg_Nebula_-_Magical_Forest/download',
+      genre: 'Jungle',
+      color: ['#05D36B', '#00B0FD'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Totem',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2FEgg_Nebula_-_We_Are_Humanity_And_We_Are_Lost_In_The_Cosmic_Infinity_-_20140518210441755.jpg&width=290&height=290&type=image',
+      artist: 'Egg Nebula',
+      audio: 'https://freemusicarchive.org/track/06_Egg_Nebula_-_Totem/download',
+      genre: 'Jungle',
+      color: ['#05D36B', '#00B0FD'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'To Survive',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2FUno_Datwangi_-_In_Sequence_-_20150906200548412.jpg&width=290&height=290&type=image',
+      artist: 'Uno Datwangi',
+      audio: 'https://freemusicarchive.org/track/4_To_Survive/download',
+      genre: 'Jungle',
+      color: ['#605C3F', '#620733'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Hear To Take Your Heart Away',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2Fjonas_the_plugexpert_-_Den_Haag_-_Nijmegen_-_2013070230952721.jpg&width=290&height=290&type=image',
+      artist: 'jonas the plugexpert',
+      audio:
+        'https://freemusicarchive.org/track/Jonas_The_Plugexpert_-_Den_Haag_-_Nijmegen_-_07_Hear_To_Take_Your_Heart_Away/download',
+      genre: 'Jungle',
+      color: ['#E7E0D0', '#957584'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Succubus',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2FCoven_Cvlt_-_Midnight_-_2018100131058095.jpg&width=290&height=290&type=image',
+      artist: 'Coven Cvlt',
+      audio:
+        'https://freemusicarchive.org/track/02Coven-Succubus-Midnight-SLC40_Soisloscerdos/download',
+      genre: 'Techno',
+      color: ['#170C14', '#76055A'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Unknown Orchestra',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2F029_-_Net_-_20180711105550796.JPG&width=290&height=290&type=image',
+      artist: '(029)',
+      audio:
+        'https://freemusicarchive.org/track/03029-Unknown_Orchestra-Net-Soisloscerdos-SLC39/download',
+      genre: 'Techno',
+      color: ['#010101', '#9D9D9D'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Galvan',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2F029_-_Net_-_20180711105550796.JPG&width=290&height=290&type=image',
+      artist: '(029)',
+      audio: 'https://freemusicarchive.org/track/02029-Galvan-Net-Soisloscerdos-SLC39/download',
+      genre: 'Techno',
+      color: ['#010101', '#9D9D9D'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Busted',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2F029_-_Net_-_20180711105550796.JPG&width=290&height=290&type=image',
+      artist: '(029)',
+      audio: 'https://freemusicarchive.org/track/01029-Busted-Net-Soisloscerdos-SLC39/download',
+      genre: 'Techno',
+      color: ['#010101', '#9D9D9D'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Minotauro',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2FLangax_-_Acid_Myths_-_2018070360213484.jpg&width=290&height=290&type=image',
+      artist: 'Langax',
+      audio: 'https://freemusicarchive.org/track/01Langax-Minotauro-Acid_Myths-SLC38/download',
+      genre: 'Techno',
+      color: ['#F8FC01', '#FFFC05'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'De la Rue',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2F029_-_Backwards_EP_-_2018020865523393.png&width=290&height=290&type=image',
+      artist: '(029)',
+      audio: 'https://freemusicarchive.org/track/04029-De_la_Rue-Backwards_EP-SLC34/download',
+      genre: 'Techno',
+      color: ['#FDFDFE', '#828CC8'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Texturas',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2F029_-_Backwards_EP_-_2018020865523393.png&width=290&height=290&type=image',
+      artist: '(029)',
+      audio: 'https://freemusicarchive.org/track/02029-Texturas-Backwards_EP-SLC34/download',
+      genre: 'Techno',
+      color: ['#FDFDFE', '#828CC8'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Tribal',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2F029_-_Backwards_EP_-_2018020865523393.png&width=290&height=290&type=image',
+      artist: '(029)',
+      audio: 'https://freemusicarchive.org/track/03029-Tribal-Backwards_EP-SLC34/download',
+      genre: 'Techno',
+      color: ['#FDFDFE', '#828CC8'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Euritmix Sux My Dix',
+      cover:
+        'https://freemusicarchive.org/image?file=images/albums/Computer_Truck_-_Rock_The_Boulevard_Reach_The_Bourgeois_-_2010011590226089.jpeg&width=290&height=290&type=image',
+      artist: 'Computer Truck',
+      audio: 'https://freemusicarchive.org/track/06_-_Euritmix_Sux_My_Dix/download',
+      genre: 'Breakcore',
+      color: ['#A1A6AA', '#124F5A'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Escape Velocity',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2FMetalogue_-_Paroxysm_-_20180227131431242.jpg&width=290&height=290&type=image',
+      artist: 'Metalogue',
+      audio:
+        'https://freemusicarchive.org/track/Metalogue_-_Paroxysm_-_04_Escape_Velocity/download',
+      genre: 'Breakcore',
+      color: ['#FEFFFF', '#5D4032'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Technical Debt',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2FMetalogue_-_Paroxysm_-_20180227131431242.jpg&width=290&height=290&type=image',
+      artist: 'Metalogue',
+      audio: 'https://freemusicarchive.org/track/Metalogue_-_Paroxysm_-_03_Technical_Debt/download',
+      genre: 'Breakcore',
+      color: ['#FEFFFF', '#5D4032'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Screw Base',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2FChipnDamned_Records_-_Bleep_or_Die_-_20100719160305716.jpg&width=290&height=290&type=image',
+      artist: 'Beytah',
+      audio: 'https://freemusicarchive.org/track/10_-_Beytah_-_Screw_Base/download',
+      genre: 'Breakcore',
+      color: ['#01FFFF', '#FDFB01'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'The Lord Of The Old ScoolIntroducng The Old Testament Style',
+      cover:
+        'https://freemusicarchive.org/image?file=images%2Falbums%2FDJ_Black_Negro_-_Thunder_Doom_-_20131207122410967.jpg&width=290&height=290&type=image',
+      artist: 'The Lord Of The Old Scool',
+      audio:
+        'https://freemusicarchive.org/track/03_-_The_Lord_Of_The_Old_Scool_-_Introducng_The_Old_Testament_Style/download',
+      genre: 'Breakcore',
+      color: ['#000000', '#C50668'],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+};
